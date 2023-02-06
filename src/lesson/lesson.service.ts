704c0da659798dced9ed561b37918645cf795111
yuriagopian <yuri.agopian@hotmail.com>
2023-02-06 14:49:56 -0300

    chore: Lesson Service

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Lesson } from './lesson.entity';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson)
+    private lessonRepository: Repository<Lesson>,
+  ) {}
+}
