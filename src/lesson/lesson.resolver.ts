5bad0eb82483e76a12f006ded2f045130dc6e835
yuriagopian <yuri.agopian@hotmail.com>
2023-02-06 17:46:07 -0300

    chore: Lesson Service

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,18 +1,22 @@
-import { Resolver, Query, Mutation } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: 'kpasokoas',
-      name: 'Phasgas',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation((returns) => LessonType)
-  createLesson() {}
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
 }
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -11,6 +11,10 @@ export class LessonService {
     private lessonRepository: Repository<Lesson>,
   ) {}
 
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ where: { id } });
+  }
+
   async createLesson(name, startDate, endDate): Promise<Lesson> {
     const id = uuid();
     const lesson = this.lessonRepository.create({
