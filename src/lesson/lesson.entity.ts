f58242e615e2f37b481473c075a09083751bbd9c
yuriagopian <yuri.agopian@hotmail.com>
2023-02-06 14:37:18 -0300

    chore: Lesson entity

--- src/lesson/lesson.entity.ts
+++ src/lesson/lesson.entity.ts
@@ -1,12 +1,16 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Lesson {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   startDate: string;
 
+  @Column()
   endDate: string;
 }
