b8b169e3929077020488b16d1808657b27349427
yuriagopian <yuri.agopian@hotmail.com>
2023-02-07 16:02:36 -0300

    feat: Implements lesson students assign

--- src/lesson/assign-students-to-lesson.type.ts
+++ src/lesson/assign-students-to-lesson.type.ts
@@ -1,9 +1,14 @@
 import { Field, ID, InputType } from '@nestjs/graphql';
+import { IsUUID } from 'class-validator';
 @InputType()
 export class AssignStudentsToLessonInput {
+  @IsUUID()
   @Field((type) => ID)
   lessonId: string;
 
+  @IsUUID('4', {
+    each: true,
+  })
   @Field((type) => [ID])
   studentsIds: string[];
 }
